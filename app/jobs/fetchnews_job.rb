3d113882e1f2866bd529c0db078b53bffe278e2d
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-23 23:25:19 +0200

    added job method

--- app/jobs/fetchnews_job.rb
+++ app/jobs/fetchnews_job.rb
@@ -4,4 +4,19 @@ class FetchnewsJob < ActiveJob::Base
   def perform(*args)
     # Do something later
   end
+
+  def get_news
+    url = "https://en.wikipedia.org/wiki/Portal:Current_events"
+    page = Nokogiri::HTML(open(url))
+
+    news = page.css("table")[1]
+
+    events = news.css("ul")[0]
+    events_list = []
+
+    events.css("li").each do |elements|
+      events_list << elements.text
+    end
+    events_list
+  end
 end
