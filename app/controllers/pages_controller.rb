b3683c2dbf536e5b55ade9665e02c8dc4f01c7e5
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-23 17:00:20 +0200

    added index of news

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,25 @@
+require 'open-uri'
+require 'nokogiri'
+
 class PagesController < ApplicationController
+  def index
+    @test = 2+2
+    @events = get_news
+  end
+
+  def get_news
+    url = "https://en.wikipedia.org/wiki/Portal:Current_events"
+    page = Nokogiri::HTML(open(url))
+
+    news = page.css("table")[1]
+
+    events = news.css("ul")[0]
+    events_list = []
+
+    events.css("li").each do |elements|
+      events_list << elements.text
+    end
+    events_list
+  end
+
 end
