eeed44487fe528f33150a68200cd678d2d2af989
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-10-05 09:38:54 +0200

    update news

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'cgi'
 
 class Story < ActiveRecord::Base
 
@@ -7,7 +8,10 @@ class Story < ActiveRecord::Base
     url = "https://en.wikipedia.org/wiki/Portal:Current_events"
     page = Nokogiri::HTML(open(url))
 
+
     news = page.css("table")[1]
+
+    links = news.css("a")
 
     events = news.css("ul")[0]
     events_list = []
@@ -18,9 +22,15 @@ class Story < ActiveRecord::Base
     end
   end
 
+  def wikify_link
+    news.css("a").each do |link|
+      puts link.attributes["href"].value = "https://en.wikipedia.org#{CGI.escape link.attributes["href"].value}"
+    end
+  end
 end
 
 
 # Play with Wikipedia's API
 # http://en.wikipedia.org/w/api.php?action=query&titles=Ile d'Yeu&format=json
 # https://en.wikipedia.org/w/index.php?title=%C3%8Ele_d%27Yeu&curid=630868
+# .gsub(/\/wiki\//, "http://en.wikipedia.org/wiki/")
